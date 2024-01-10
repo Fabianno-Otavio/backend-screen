457b01b24abf77466d38538446a856bdf1de662a
Fabianno Otávio <fabiannootavio08@gmail.com>
2024-01-10 19:32:27 -0300

    feature :sparkles: | added-users-api

---
+++ src/user/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UserController } from './user.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { UserService } from './user.service';
+
+@Module({
+    imports: [PrismaModule],
+    exports: [UserService],
+    controllers: [UserController],
+    providers: [UserService],
+})
+export class UserModule {}
