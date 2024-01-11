1321d6a1ba50be4391ec6b4c9a2dbec877344065
Fabianno Otávio <fabiannootavio08@gmail.com>
2024-01-11 12:32:27 -0300

    feature :sparkles: | added-auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,69 @@
+import * as bcrypt from 'bcrypt';
+import { GetUserDto } from 'src/user/dto/get-user.dto';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import {
+    BadRequestException,
+    Injectable,
+    UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private readonly prisma: PrismaService,
+        private readonly jwtService: JwtService,
+    ) {}
+
+    public async login(data: {
+        username: string;
+        password: string;
+    }): Promise<{ access_token: string }> {
+        const user = await this.prisma.users.findFirst({
+            where: {
+                username: data.username,
+            },
+        });
+
+        if (!user) {
+            throw new UnauthorizedException('E-mail e/ou senha inválidos.');
+        } else if (!(await bcrypt.compare(data.password, user.password))) {
+            throw new UnauthorizedException('E-mail e/ou senha inválidos.');
+        }
+
+        return this.createToken(user);
+    }
+
+    private createToken(user: GetUserDto): { access_token: string } {
+        const payload = {
+            ...user,
+            role: 'user',
+        };
+
+        return {
+            access_token: this.jwtService.sign(payload, {
+                secret: process.env.JWT_SECRET,
+                expiresIn: '30d',
+                issuer: 'auth/login',
+                audience: 'users',
+            }),
+        };
+    }
+
+    public checkToken(token: string): {
+        id: string;
+        username: string;
+        email: string;
+        role: string;
+    } {
+        try {
+            return this.jwtService.verify(token, {
+                secret: process.env.JWT_SECRET,
+                issuer: 'auth/login',
+                audience: 'users',
+            });
+        } catch (e) {
+            throw new BadRequestException('Token inválido.');
+        }
+    }
+}
--- src/user/dto/get-user.dto.ts
+++ src/user/dto/get-user.dto.ts
@@ -1,6 +1,6 @@
 import { OmitType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
 
-export class GetUserDto extends OmitType(CreateUserDto, [
-    'password',
-] as const) {}
+export class GetUserDto extends OmitType(CreateUserDto, ['password'] as const) {
+    id: string;
+}
