ce801c3e57e8faf01c0ff603f71ef5203fdd3311
Fabianno Otávio <fabiannootavio08@gmail.com>
2024-01-15 15:46:19 -0300

    hotfix | fixed item module imports

--- src/item/item.module.ts
+++ src/item/item.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { AuthService } from 'src/auth/auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  providers: [ItemService],
-  controllers: [ItemController]
+    imports: [PrismaModule, JwtModule, UserModule],
+    providers: [ItemService, AuthService],
+    controllers: [ItemController],
 })
 export class ItemModule {}
